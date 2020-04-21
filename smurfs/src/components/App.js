4aa5c39fd81cfe3cc1a37e29f97ec70895bdbc16
aaronmatson1 <matsonaaron1@gmail.com>
2020-04-20 20:31:06 -0600

    Quick CSS fix

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 
 import { connect } from "react-redux";
 import axios from "axios";
+import styled from 'styled-components'
 
 import { fetchData } from "../actions";
 import Card from "./Card";
 import ItemForm from "./ItemForm";
+
+
+const CardContainer = styled.div`
+    display: flex;
+    width: 800px;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin: 20px auto;
+`;
 
 class App extends Component {
 
@@ -33,7 +43,9 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <CardContainer>
         {this.state.data && this.state.data.map(data => <Card key={data.id} {...data} />)}
+        </CardContainer>
         <ItemForm />
       </div>
     );
